0943a07ff419bc3165313645bb9824410061769f
Fabien Huitelec <fabien.huitelec@gmail.com>
2013-12-17 22:36:42 +0100

    commit initial pour la branche a_merge

---
+++ controler/Controler.java
@@ -0,0 +1,22 @@
+package controler;
+
+import java.awt.event.ActionEvent;
+
+import fenetre.Fenetre_principale;
+
+public class Controler {
+	private Fenetre_principale fenetre;
+	
+	public Controler(Fenetre_principale fenetre) {
+		super();
+		this.fenetre = fenetre;
+	}
+	
+	public void actionPerformed(ActionEvent e) {				
+		// QUITTER
+		System.out.println(e.getActionCommand());
+		if (e.getActionCommand().equals("Exit") ) {
+			System.out.println("Quitter");
+		}
+	}
+}
---
+++ fenetre/MenuG.java
@@ -0,0 +1,56 @@
+package fenetre;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MenuG extends JPanel{
+
+	JButton crayon;
+	JButton	rectangle;
+	JButton	cercle;
+	
+	
+	
+	public MenuG(){
+		ImageIcon imgrectangle = new ImageIcon("carre.jpg");
+		ImageIcon imgcercle = new ImageIcon("cercle.jpg");
+		ImageIcon imgcrayon = new ImageIcon("crayon.jpg");
+
+		crayon=new JButton(imgcrayon);
+		crayon.setPreferredSize(new Dimension(29,29));
+	
+		rectangle=new JButton(imgrectangle);
+		rectangle.setPreferredSize(new Dimension(29,29));
+
+		cercle=new JButton(imgcercle);
+		cercle.setPreferredSize(new Dimension(29,29));
+
+
+		GridLayout grid = new GridLayout(2,3);
+		setLayout(grid);
+		add(crayon,grid);
+		add(rectangle,grid);
+		add(cercle,grid);
+		
+		//this.setMaximumSize(new Dimension(29*2,5*29));
+		
+
+	}
+
+	/* MAIN D'ALEX */
+	public static void  main (String args[]){
+		JFrame fram = new JFrame();
+		JPanel menuG = new MenuG();
+		fram.add(BorderLayout.WEST,menuG);
+		
+		fram.pack();
+		fram.setVisible(true);
+	}
+}
+
