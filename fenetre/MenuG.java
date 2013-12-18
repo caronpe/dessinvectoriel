6b098417db3c8f7730333be5bf6e8bab46d45646
Fabien Huitelec <fabien.huitelec@gmail.com>
2013-12-18 20:04:34 +0100

    Réglage de bug d'affichage de la barre d'outils (MenuG) et commentaires dans MenuG

--- fenetre/MenuG.java
+++ fenetre/MenuG.java
@@ -3,6 +3,7 @@ package fenetre;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.File;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -14,33 +15,37 @@ import javax.swing.JPanel;
 import javax.swing.colorchooser.AbstractColorChooserPanel;
 
 public class MenuG extends JPanel{
-
-	JButton crayon;
-	JButton	rectangle;
-	JButton	cercle;
-	JButton rouge;
-	JButton bleu;
-	JButton vert;
 	BoxLayout box1;
+	JColorChooser bacacouleur;
+	// JButton
+		JButton crayon;
+		JButton	rectangle;
+		JButton	cercle;
+		JButton rouge;
+		JButton bleu;
+		JButton vert;
+	// ImageIcon
 	ImageIcon imgrectangle;
 	ImageIcon imgcercle;
 	ImageIcon imgcrayon;
-	JColorChooser bacacouleur;
-	
+		
 	public MenuG(){
-		imgrectangle = new ImageIcon("carre.jpg");
-		imgcercle = new ImageIcon("cercle.png");
-		imgcrayon = new ImageIcon("crayon.jpg");
-		 
-		this.setPreferredSize(new Dimension(45,45));
-		this.setMinimumSize(new Dimension(45,45));
-		this.setMaximumSize(new Dimension(45,45));
+		initialisation();
+		
+		// Définition de l'adresse de chaque icône
+		imgrectangle = new ImageIcon("src_projet/ressources/carre.jpg");
+		imgcercle = new ImageIcon("src_projet/ressources/cercle.png");
+		imgcrayon = new ImageIcon("src_projet/ressources/crayon.jpg");
+		
+		// Outils
 		crayon = new JButton(imgcrayon);
 		crayon.setPreferredSize(new Dimension(22,22));
 		rectangle = new JButton(imgrectangle);
 		rectangle.setPreferredSize(new Dimension(22,22));
 		cercle = new JButton(imgcercle);
 		cercle.setPreferredSize(new Dimension(22,22));
+		
+		// Couleurs
 		rouge= new JButton();
 		rouge.setBackground(Color.RED);
 		rouge.setPreferredSize(new Dimension(22,22));
@@ -51,12 +56,20 @@ public class MenuG extends JPanel{
 		vert.setBackground(Color.GREEN);
 		vert.setPreferredSize(new Dimension(22,22));
 		
+		// Ajouts outils
 		add(crayon);
 		add(rectangle);
 		add(cercle);
-		add(crayon);
-		add(rectangle);
-		add(cercle);
+		// Ajours couleurs
+		add(rouge);
+		add(bleu);
+		add(vert);
+	}
+	
+	public void initialisation() {
+		this.setPreferredSize(new Dimension(45,45));
+		this.setMinimumSize(new Dimension(45,45));
+		this.setMaximumSize(new Dimension(45,45));
 	}
 }
 
