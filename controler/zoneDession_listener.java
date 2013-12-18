7bd3874a00bd405aab9a79f0a19ebd129e445e94
Caron <pierre-edouard_caron@hotmail.com>
2013-12-18 15:51:34 +0100

    creation d'un controleur pour la zone de dessin

---
+++ controler/zoneDession_listener.java
@@ -0,0 +1,38 @@
+package controler;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class zoneDession_listener implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
